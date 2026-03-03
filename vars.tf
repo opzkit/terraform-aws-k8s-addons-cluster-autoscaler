8d017cde06d8d54a57703bb768e85214e5e15e57
Peter Svensson <peter@sparetimecoders.com>
2026-03-03 15:28:33 +0100

    feat: parameterize balance_similar_node_groups for autoscaler
    Update addon_content.tpl to use a variable for 
    balance_similar_node_groups, allowing for dynamic configuration. 
    Add the variable to vars.tf and adjust outputs.tf to 
    pass this new variable into the template. These changes 
    support enhanced flexibility for managing cluster autoscaling 
    behavior.

--- vars.tf
+++ vars.tf
@@ -8,3 +8,9 @@ variable "cluster_name" {
   type        = string
   description = "Name of k8s cluster"
 }
+
+variable "balance_similar_node_groups" {
+  type        = bool
+  default     = true
+  description = "If you set the flag to true, CA will automatically identify node groups with the same instance type and the same set of labels (except for automatically added zone label) and try to keep the sizes of those node groups balanced"
+}
